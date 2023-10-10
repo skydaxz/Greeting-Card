19b174d1c72131acbdf91b8cc4e6ac8f30806ec0
skydaxz <kevin.vasco037@gmail.com>
2023-10-10 13:07:06 +0200

    code

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Kevin Vasco")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color(255, 153, 0)) {
+        Text(
+            text = "Hola, me llamo $name",
+            modifier = modifier.padding(20.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Kevin Vasco")
+    }
+}
